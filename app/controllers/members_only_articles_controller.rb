8c1c47099e8f227b1c9105dcebad78c088e61a32
Brigham <brig.johnson@gmail.com>
2022-07-10 23:29:35 -0400

    commit tree 1c18a376ede31e93d50d1d004d9b7e80f3c98fd9

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,12 +1,17 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorize
 
   def index
+    # return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
   end
 
   def show
+    # return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+
     article = Article.find(params[:id])
     render json: article
   end
@@ -17,4 +22,9 @@ class MembersOnlyArticlesController < ApplicationController
     render json: { error: "Article not found" }, status: :not_found
   end
 
+  def authorize
+    return if session.include? :user_id
+    render json: { error: "Not authorized" }, status: :unauthorized
+    # return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
+  end
 end
